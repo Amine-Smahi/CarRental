53c1377d88f515922d2147be0007148becf8fe39
Amine Smahi <mohammed.amine.smahi@gmail.com>
2017-05-12 00:55:43 +0100

    make it more general

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 	$host = "localhost";
-	$user = "id1594239_root";
-	$pass = "Fanki480";
-	$db = "id1594239_cars";
+	$user = "";
+	$pass = "";
+	$db = "";
 	
 	$conn = new mysqli($host, $user, $pass, $db);
 	if($conn->connect_error){
