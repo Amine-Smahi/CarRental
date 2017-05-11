51563f5e1c8ad814ef87dce384124a8347d21fcb
Amine Smahi <mohammed.amine.smahi@gmail.com>
2017-05-12 00:44:30 +0100

    add images and style and javascript constrol

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+
+
+
+	/*  Hamburger Menu & Icon  */
+	$('.hamburger').on('click', function(e){
+		
+		e.preventDefault();
+		$(this).toggleClass('opned');
+		$('header nav').toggleClass('active');
+		
+	});
+
+
+
+
+	/*  Advanced search form & Icon  */
+	$('#advanced_search_btn').on("click", function(e){
+		e.preventDefault();
+
+		var ads_box =$('.advanced_search');
+		
+		if(!ads_box.hasClass('advanced_displayed')){
+
+			$(this).addClass('active');
+			ads_box.stop().fadeIn(200).addClass('advanced_displayed');
+
+		}else{
+
+			$(this).removeClass('active');
+			ads_box.stop().fadeOut(200).removeClass('advanced_displayed');
+
+		}
+
+	});
+
+
+
+
+});
